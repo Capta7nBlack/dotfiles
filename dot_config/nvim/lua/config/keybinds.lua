42ffd595648a72c6818e47b779de5a183cdb440e
Capta7nBlack <sayatmushkin@gmail.com>
2026-05-19 16:50:15 +0500

    New Plugin - Oil; Also, cleaning up the nvim config

--- dot_config/nvim/lua/config/keybinds.lua
+++ dot_config/nvim/lua/config/keybinds.lua
@@ -8,12 +8,6 @@ g.maplocalleader = " "
 ---- Explorer Opening Shortcut
 keymap.set('n', '<leader>ee', ':30vs.<CR>', { noremap = true, silent = true })
 
--------- Explorer to Config Folder
-keymap.set('n', '<leader>ec', ':EditConfig<CR>', { noremap = true, silent = true })
-
--------- Explorer to disk D
-keymap.set('n', '<leader>ed', ':Cdd<CR>', { noremap = true, silent = true })
-
 
 -- Quit and Save Shortcuts
 ---- Quit Regardless
@@ -24,9 +18,6 @@ keymap.set('n', '<leader>ww' , ':w<CR>' , { noremap = true, silent = true})
 
 -------- Write and Quite
 keymap.set('n', '<leader>wq' , ':wq<CR>' , { noremap = true, silent = true})
-
--------- Quit All Regardless
-keymap.set('n', '<leader>Q' , ':qa!<CR>' , { noremap = true, silent = true})
 
 
 -- Buffer Management Shortcuts
